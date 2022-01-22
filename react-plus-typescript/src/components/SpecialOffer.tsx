1f817918d1eaedf2f1fb5c1b3af6a4689ab9edb0
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2022-01-22 22:27:18 +0300

    Creating a Render Props component

--- react-plus-typescript/src/components/SpecialOffer.tsx
+++ react-plus-typescript/src/components/SpecialOffer.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 
-import { withAddToCart, AddToCartProps } from "./AddToCart";
+import { WithAddToCartProps } from "./AddToCart";
 
 import { Pizza } from "./types";
 
 import SpecialOfferCSS from "./SpecialOffer.module.css";
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: Pizza;
 }
 
-const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCart = () => {
-    addToCart({ id: pizza.id, name: pizza.name, price: pizza.price });
-  };
+const SpecialOffer: React.FC<Props> = ({ pizza }) => (
+  <div className={SpecialOfferCSS.container}>
+    <h2>{pizza.name}</h2>
+    <p>{pizza.description}</p>
+    <p>{pizza.price}</p>
+    <WithAddToCartProps>
+      {({ addToCart }) => (
+        <button
+          type="button"
+          onClick={() =>
+            addToCart({ id: pizza.id, name: pizza.name, price: pizza.price })
+          }
+        >
+          Add to Cart
+        </button>
+      )}
+    </WithAddToCartProps>
+  </div>
+);
 
-  return (
-    <div className={SpecialOfferCSS.container}>
-      <h2>{pizza.name}</h2>
-      <p>{pizza.description}</p>
-      <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCart}>
-        Add to Cart
-      </button>
-    </div>
-  );
-};
-
-export default withAddToCart(SpecialOffer);
+export default SpecialOffer;
