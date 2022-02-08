6b8156193a7106949e90b28ed20941bf2974397e
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2022-02-08 17:03:38 +0300

    Filtering tasks by task status

--- task-mate/generated/graphql-frontend.ts
+++ task-mate/generated/graphql-frontend.ts
@@ -114,7 +114,9 @@ export type TaskQuery = (
   )> }
 );
 
-export type TasksQueryVariables = Exact<{ [key: string]: never; }>;
+export type TasksQueryVariables = Exact<{
+  status?: Maybe<TaskStatus>;
+}>;
 
 
 export type TasksQuery = (
@@ -247,8 +249,8 @@ export type TaskQueryHookResult = ReturnType<typeof useTaskQuery>;
 export type TaskLazyQueryHookResult = ReturnType<typeof useTaskLazyQuery>;
 export type TaskQueryResult = Apollo.QueryResult<TaskQuery, TaskQueryVariables>;
 export const TasksDocument = gql`
-    query Tasks {
-  tasks {
+    query Tasks($status: TaskStatus) {
+  tasks(status: $status) {
     id
     title
     status
@@ -268,6 +270,7 @@ export const TasksDocument = gql`
  * @example
  * const { data, loading, error } = useTasksQuery({
  *   variables: {
+ *      status: // value for 'status'
  *   },
  * });
  */
--- task-mate/pages/update/[id].tsx
+++ task-mate/pages/update/[id].tsx
@@ -17,12 +17,12 @@ const UpdateTask: React.FC = () => {
   const id =
     typeof router.query?.id === "string" ? parseInt(router.query?.id, 10) : NaN;
 
-  const { data, loading, error } = useTaskQuery({ variables: { id } });
-  const task = data?.task;
-
   if (!id) {
     return <Error statusCode={404} />;
   }
+
+  const { data, loading, error } = useTaskQuery({ variables: { id } });
+  const task = data?.task;
 
   return loading ? (
     <p>Loading ...</p>
